e9987b17e33fbbce5e9670c8bf80d1d8f08a7ba9
mhlee-icml <mh.lee@inu.ac.kr>
2026-03-18 11:07:17 +0000

    Deploying to gh-pages from @ mhlee-icml/mhlee-icml.github.io@62dc9965eb957a678e22df04a983fdc772ae4f07 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://woojinJeong52.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://mhlee-imcl.github.io/#contact">contact us</a>.'
               }
             ]
           }
